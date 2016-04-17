4570c2367f9db92ac9039ad644db64577013c68e
Shinae Woo <shinae2012@gmail.com>
2016-04-17 22:53:42 +0900

    Loadbalacing packet by hasing + modular

--- core/modules/load_balance.c
+++ core/modules/load_balance.c
@@ -7,20 +7,49 @@
 #include <rte_tcp.h>
 
 #include "../module.h"
+#include "md5.h"
 
 /* XXX: currently doesn't support multiple workers */
 
 /* ngates: number of lb clients 
  * gates[0] ~ ngates[ngates-1]: ports for lb clients
  */
+
+#define MAX_BUCKETS 100
 struct lb_priv {
 	gate_t gates[MAX_OUTPUT_GATES];
 	struct {
 		uint16_t offset;
-		uint32_t mask;
+		uint32_t size;
 	} rule;
 	uint32_t ngates;
+
+#ifdef CHASHING
+	uint32_t nbuckets;
+	uint32_t hash_points[MAX_OUTPUT_GATES][MAX_BUCKETS];
+#endif
 };
+
+#ifdef CHASHING
+static uint32_t _hash(int a, int b) 
+{
+	uint32_t hash = 0;
+	unsigned char result[16];
+
+	MD5_CTX ctx;
+	MD5_Init(&ctx);
+	MD5_Update(&ctx, &a, 4);
+	MD5_Update(&ctx, &b, 4);
+	MD5_Final(result, &ctx);
+
+	hash = result[1] +
+		(result[3] << 4) +
+		(result[7] << 8) +
+		(result[11] << 16);
+
+	return hash % UINT32_MAX;
+}
+#endif
 
 static struct snobj *lb_init(struct module *m, struct snobj *arg)
 {	
@@ -31,9 +60,10 @@ static struct snobj *lb_init(struct module *m, struct snobj *arg)
 	if (snobj_eval_exists(arg, "gates") &&
 	      snobj_eval(arg, "gates")->type == TYPE_INT) {
 		int gate = snobj_eval_int(arg, "gates");
-		if (gate > MAX_OUTPUT_GATES)
-			return snobj_err(EINVAL, "No more than %d gates", 
-					MAX_OUTPUT_GATES-1);
+		if (gate > MAX_OUTPUT_GATES || gate < 0)
+			return snobj_err(EINVAL, "No more than %d gates or "
+					"no less than 0 gates", 
+					MAX_OUTPUT_GATES);
 		priv->ngates = gate;
 		for (int i = 0; i < gate; i++) {
 			priv->gates[i] = i;
@@ -63,23 +93,33 @@ static struct snobj *lb_init(struct module *m, struct snobj *arg)
 		struct snobj *rule = snobj_eval(arg, "rule");
 
 		priv->rule.offset = snobj_eval_int(rule, "offset");
-		switch(snobj_eval_int(rule, "size")) {
-		case 1:
-			priv->rule.mask = 0x000000ff;
-			break;
-		case 2:
-			priv->rule.mask = 0x0000ffff;
-			break;
-		case 4:
-			priv->rule.mask = 0xffffffff;
-			break;
-		default:
-			return snobj_err(EINVAL, "'size' must be 1, 2, or 4");
-		}
+		priv->rule.size = snobj_eval_int(rule, "size");
 
 	} else {
 		return snobj_err(EINVAL, "Must specify rules to load balancer");
 	}
+
+#ifdef CHASHING	
+	int i, j;
+	if (snobj_eval_exists(arg, "buckets") &&
+			snobj_eval(arg, "buckets")->type == TYPE_INT) {
+		int nbuckets = snobj_eval_int(arg, "buckets");
+		if (nbuckets > MAX_BUCKETS || nbuckets < 0)
+			return snobj_err(EINVAL, "No more than %d gates or "
+					"no less than 0 gates", MAX_OUTPUT_GATES-1);
+		priv->nbuckets = nbuckets;
+	} else {
+		priv->nbuckets = 1;
+	}
+
+	for (i = 0; i < priv->ngates; ++i) {
+		for (j = 0; j < priv->nbuckets; ++j) {
+			priv->hash_points[i][j] = _hash(i, j); 
+		}
+	}
+	
+#endif
+
 	return NULL;
 	
 }
@@ -122,20 +162,22 @@ static struct snobj *lb_query(struct module *m, struct snobj *arg)
 }
 
 static inline int load_balance_pkt(uint32_t ngates, 
-		uint32_t offset, uint32_t mask, struct snbuf *snb) 
+		uint32_t offset, uint32_t size, struct snbuf *snb) 
 {
 	char *head = snb_head_data(snb);
-	uint32_t p = *(uint32_t *)(head + offset);
-	uint32_t range_per_gate = mask/ngates;
-#if 0
-	FILE *file = fopen("/home/shinae/log", "a");
-	fprintf(file, "%08x %02x %02x %02x %02x %08x %08x %d\n", mask, 
-			*(head+offset), *(head+offset+1), 
-			*(head+offset+2), *(head+offset+3), 
-			p, p & mask, (p & mask) / range_per_gate);
-	fclose(file);
-#endif
-	return (p & mask) / range_per_gate;
+	uint32_t hash = 0;
+	for (int i = 0; i < size; ++i) {
+		hash += *(head + offset + i);
+		hash += (hash << 10);
+		hash ^= (hash << 6);
+	}
+
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+
+	// no consistent hashing
+	return hash % ngates;
 }
 
 static void
@@ -146,7 +188,7 @@ lb_process_batch(struct module *m, struct pkt_batch *batch)
 
 	for (int i = 0; i < batch->cnt; i++) {
 		int gate_id = load_balance_pkt(priv->ngates, priv->rule.offset, 
-				priv->rule.mask, batch->pkts[i]);
+				priv->rule.size, batch->pkts[i]);
 		if (gate_id < 0 || gate_id > priv->ngates)
 			ogates[i] = priv->gates[priv->ngates];
 		else

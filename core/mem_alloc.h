fc7b903ee6bdf7fe142dae235a63c8791c66ade2
Shinae Woo <shinae2012@gmail.com>
2016-08-11 21:11:37 -0700

    Make compile native_app

--- core/mem_alloc.h
+++ core/mem_alloc.h
@@ -6,10 +6,58 @@
 
 #include <stddef.h>
 
-void *mem_alloc(size_t size);	/* zero initialized by default */
-void *mem_realloc(void *ptr, size_t size);
-void mem_free(void *ptr);
+#define LIBC			0
+#define DPDK			1
 
-/* void *mem_alloc_ex(size_t size, size_t align, int socket); */
+/* either LIBC or DPDK */
+#define MEM_ALLOC_PROVIDER	DPDK
 
-#endif
+#if MEM_ALLOC_PROVIDER == LIBC
+
+#include <stdlib.h>
+#include <string.h>
+
+/* zero initialized by default */
+static void *mem_alloc(size_t size)
+{
+	void *ptr = malloc(size);
+
+	if (ptr)
+		memset(ptr, 0, size);
+
+	return ptr;
+}
+
+static void *mem_realloc(void *ptr, size_t size)
+{
+	return realloc(ptr, size);
+}
+
+static void mem_free(void *ptr)
+{
+	free(ptr);
+}
+
+#elif MEM_ALLOC_PROVIDER == DPDK
+
+#include <rte_config.h>
+#include <rte_malloc.h>
+
+static void *mem_alloc(size_t size)
+{
+	return rte_zmalloc(/* name= */ NULL, size, /* align= */ 0);
+}
+
+static void *mem_realloc(void *ptr, size_t size)
+{
+	return rte_realloc(ptr, size, /* align= */ 0);
+}
+
+static void mem_free(void *ptr)
+{
+	rte_free(ptr);
+}
+
+#endif /* end of #if MEM_ALLOC_PROVIDER */
+
+#endif /* end of #ifndef _MEM_ALLOC_H */
